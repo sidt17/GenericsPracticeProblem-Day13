8bed14ecf6dc9c1c679496697cc305620203bd29
Siddhant <santoshshrivastava39@gmail.com>
2022-03-17 17:14:09 +0530

    Updated for TC-2.3

--- Program.cs
+++ Program.cs
@@ -6,8 +6,8 @@ namespace Generics
         public static void Main(string[] args)
         { 
             Checkmax value = new Checkmax();
-            float d = value.Maxnumber(3.85f, 4.563f, 2.576f);
-            Console.WriteLine(d + " is the largest number at second position");
+            float d = value.Maxnumber(3.85f, 4.563f, 8.6454f);
+            Console.WriteLine(d + " is the largest number at third position");
         }
     }
 }
