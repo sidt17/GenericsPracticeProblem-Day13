c570af9094277204a113f2a5868cebd8c678effe
Siddhant <santoshshrivastava39@gmail.com>
2022-03-18 00:21:25 +0530

    Updated for TC-3.1

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ namespace Generics
     public class Program
     {
         public static void Main(string[] args)
-        { 
+        {
             Checkmax value = new Checkmax();
-             value.Maxnumber();
-;         
+            string d = value.Maxnumber("Peach", "Apple", "Banana");
+            Console.WriteLine("The max string at first position is " + d);
         }
     }
 }
+                                                                                                                                                                                                                                                                                                                                                                                                                                                       
