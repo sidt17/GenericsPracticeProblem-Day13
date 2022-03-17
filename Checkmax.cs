274986bf7415faa2402fcbfe571429e2fce6bf0a
Siddhant <santoshshrivastava39@gmail.com>
2022-03-17 15:59:08 +0530

    TC-1.1

--- Checkmax.cs
+++ Checkmax.cs
@@ -8,25 +8,23 @@ namespace Generics
 {
     public class Checkmax
     {
-        public void  Maxnumber()
+        public int Maxnumber(int a, int b, int c)
         {
-            int a = 50;
-            int b = 30;
-            int c = 10;
+            
             if (a.CompareTo(b) == 1 && a.CompareTo(c) == 1)      //comparing values to each other by using compareTo keyword
             {
-                Console.WriteLine("a is largest number that is " + a);
+                return a;
             }
             if (b.CompareTo(a) ==1 && b.CompareTo(c) == 1)
             {
-                Console.WriteLine("b is largest number that is " + b);
+                return b;
             }
             if (c.CompareTo(a) == 1 && c.CompareTo(b) == 1)
             {
-                Console.WriteLine("c is largest number that is " + c);
+                return c;
             }
-           
 
+            return default;
 
         }
 
