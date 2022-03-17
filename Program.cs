56c914f415d6c938b9811278bcaa3f2bf2fd6f90
Siddhant <santoshshrivastava39@gmail.com>
2022-03-17 15:38:00 +0530

    Call in main method

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Generics
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        { 
+            Checkmax value = new Checkmax();
+            value.Maxnumber();
+           
+           
+        }
+    }
+}
