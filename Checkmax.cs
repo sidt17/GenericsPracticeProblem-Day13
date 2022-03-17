c0a4084dcf877f7f448904466b2f4f482e22d47f
Siddhant <santoshshrivastava39@gmail.com>
2022-03-17 17:04:03 +0530

    Tc-1.1 solved

--- Checkmax.cs
+++ Checkmax.cs
@@ -8,24 +8,22 @@ namespace Generics
 {
     public class Checkmax
     {
-        public void Maxnumber()
+        public float Maxnumber(float a, float b, float c)
         {
-            float a = 5.64f;
-            float b = 5.43f;
-            float c = 4.74f;
+            
             if (a.CompareTo(b) == 1 && a.CompareTo(c) == 1)      //comparing values to each other by using compareTo keyword
             {
-                Console.WriteLine("a is largest number that is " + a);
+                return a;
             }
             if (b.CompareTo(a) == 1 && b.CompareTo(c) == 1)
             {
-                Console.WriteLine("b is largest number that is " + b);
+                return b;
             }
             if (c.CompareTo(a) == 1 && c.CompareTo(b) == 1)
             {
-                Console.WriteLine("c is largest number that is " + c);
+                return c;
             }
-
+            return default;
            
 
         }
