233aea56e68921a7d6e10c736d89210a8fd3bbfd
Siddhant <santoshshrivastava39@gmail.com>
2022-03-17 15:40:11 +0530

    Find the maximum integer

---
+++ Checkmax.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Checkmax
+    {
+        public void  Maxnumber()
+        {
+            int a = 50;
+            int b = 30;
+            int c = 10;
+            if (a.CompareTo(b) == 1 && a.CompareTo(c) == 1)      //comparing values to each other by using compareTo keyword
+            {
+                Console.WriteLine("a is largest number that is " + a);
+            }
+            if (b.CompareTo(a) ==1 && b.CompareTo(c) == 1)
+            {
+                Console.WriteLine("b is largest number that is " + b);
+            }
+            if (c.CompareTo(a) == 1 && c.CompareTo(b) == 1)
+            {
+                Console.WriteLine("c is largest number that is " + c);
+            }
+           
+
+
+        }
+
+
+    }
+          
+
+    
+
+
+         
+    
+    
+}
