e42e371a0c1d73b58f4949220773c5e1499261ee
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 23:20:20 +0530

    Refactor-2 Solved

--- Program.cs
+++ Program.cs
@@ -7,12 +7,12 @@ namespace Generics
     {
         static void Main(string[] args)
         {
-          
-            FindMax findMax = new FindMax();
+
+            Checkmax findMax = new Checkmax();
             Console.WriteLine("The max int value is :");
-            Console.WriteLine(findMax.MaxValue<int>(75, 48, 50));
+            Console.WriteLine(findMax.MaxValue<int>(15, 28, 10));
             Console.WriteLine("The max float value is :");
-            Console.WriteLine(findMax.MaxValue<float>(40.36F, 80.762F, 30.865F));
+            Console.WriteLine(findMax.MaxValue<float>(20.36f, 10.762f, 13.865f));
             Console.WriteLine("The max string value is :");
             Console.WriteLine(findMax.MaxValue<string>("Apple", "Peach", "Banana"));
 
