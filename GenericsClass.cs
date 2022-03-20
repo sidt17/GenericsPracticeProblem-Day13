0908d55a0df887f0c968bf999fa436264ba4e91c
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 23:21:29 +0530

    Create Generics Class

---
+++ GenericsClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class GenericsClass<T>
+    {
+        public T MaxValue<T>(T Value1, T Value2, T Value3)
+        {
+            if (Comparer<T>.Default.Compare(Value1, Value2) > 0 && Comparer<T>.Default.Compare(Value1, Value2) > 0)
+            {
+                return Value1;
+            }
+            else if (Comparer<T>.Default.Compare(Value2, Value1) > 0 && Comparer<T>.Default.Compare(Value2, Value3) > 0)
+            {
+                return Value2;
+            }
+            else
+            {
+                return Value3;
+            }
+        }
+
+
+    }
+}
