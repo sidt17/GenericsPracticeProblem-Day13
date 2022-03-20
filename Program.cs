444e00750948666a642c5722eb71317a9f4510a0
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 23:45:03 +0530

    UC-4 Solved

--- Program.cs
+++ Program.cs
@@ -1,23 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Generics
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
 
-            Checkmax findMax = new Checkmax();
-            Console.WriteLine("The max int value is :");
-            Console.WriteLine(findMax.MaxValue<int>(15, 28, 10));
-            Console.WriteLine("The max float value is :");
-            Console.WriteLine(findMax.MaxValue<float>(20.36f, 10.762f, 13.865f));
-            Console.WriteLine("The max string value is :");
-            Console.WriteLine(findMax.MaxValue<string>("Apple", "Peach", "Banana"));
+            MaxCount<int> intValue = new MaxCount<int>(32, 12, 190, 105, 50);
+            intValue.MaxValue();
 
+            MaxCount<float> floatValue = new MaxCount<float>(74.23f, 18.63f, 65.93f, 88.99f, 70.86f);
+            floatValue.MaxValue();
+
+            MaxCount<string> stringValue = new MaxCount<string>("Grapes", "Mango", "Guava", "Orange", "Pinepale");
+            stringValue.MaxValue();
         }
     }
 }
-
-
