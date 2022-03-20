3ec96ce812d0e6edee8d7f52cd6d959abe189b19
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 23:58:24 +0530

    UC-5 Solved

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ namespace Generics
         static void Main(string[] args)
         {
 
-            MaxCount<int> intValue = new MaxCount<int>(32, 12, 190, 105, 50);
-            intValue.MaxValue();
+            MaxCount<int> intValue = new MaxCount<int>(57, 28, 45, 100, 96);
+            intValue.PrintMax();
 
-            MaxCount<float> floatValue = new MaxCount<float>(74.23f, 18.63f, 65.93f, 88.99f, 70.86f);
-            floatValue.MaxValue();
+            MaxCount<float> floatValue = new MaxCount<float>(96.12f, 61.54f, 10.99f, 38.4f, 75.98f);
+            floatValue.PrintMax();
 
-            MaxCount<string> stringValue = new MaxCount<string>("Grapes", "Mango", "Guava", "Orange", "Pinepale");
-            stringValue.MaxValue();
+            MaxCount<string> stringValue = new MaxCount<string>("Grapes", "Papaya", "Orange", "Apple", "Banana");
+            stringValue.PrintMax();
         }
     }
 }
