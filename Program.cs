ec04869ff600b650bade53d848fafd0a441f9a04
Siddhant <santoshshrivastava39@gmail.com>
2022-03-20 23:06:05 +0530

    Refactor-1 Solved

--- Program.cs
+++ Program.cs
@@ -1,14 +1,23 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace Generics
 {
-    public class Program
+    internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Checkmax value = new Checkmax();
-            string d = value.Maxnumber("Apple", "Banana", "Peach");
-            Console.WriteLine("The max string at third position is " + d);
+          
+            FindMax findMax = new FindMax();
+            Console.WriteLine("The max int value is :");
+            Console.WriteLine(findMax.MaxValue<int>(75, 48, 50));
+            Console.WriteLine("The max float value is :");
+            Console.WriteLine(findMax.MaxValue<float>(40.36F, 80.762F, 30.865F));
+            Console.WriteLine("The max string value is :");
+            Console.WriteLine(findMax.MaxValue<string>("Apple", "Peach", "Banana"));
+
         }
     }
 }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+
+
